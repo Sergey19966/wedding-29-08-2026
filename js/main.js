0579cc5fb69c46f276d62ff89eea38b945dd1a1e
Sergey19966 <sergey.polyakov.ssau@yandex.ru>
2026-07-14 23:29:54 +0300

    For mobile screen fix

--- js/main.js
+++ js/main.js
@@ -1,102 +1,78 @@
 document
 .querySelectorAll('a[href^="#"]')
 .forEach(anchor => {
-
     anchor.addEventListener("click", function(e){
-
         e.preventDefault();
-
         document.querySelector(this.getAttribute("href"))
         .scrollIntoView({
-
             behavior: "smooth"
-
         });
-
     });
-
 });
 const topButton = document.getElementById("toTop");
 
 window.addEventListener("scroll", () => {
-
     if(window.scrollY > 500){
-
         topButton.style.display = "block";
-
     }else{
-
         topButton.style.display = "none";
-
     }
-
 });
 
 topButton.addEventListener("click", () => {
-
     window.scrollTo({
-
         top:0,
-
         behavior:"smooth"
-
     });
-
 });
 
 const sections = document.querySelectorAll("section");
 
 const observer = new IntersectionObserver(entries=>{
-
     entries.forEach(entry=>{
-
         if(entry.isIntersecting){
-
             entry.target.classList.add("show");
-
         }
-
     });
-
 },{
     threshold:.15
 });
 
 sections.forEach(section=>{
-
     section.classList.add("hidden");
-
     observer.observe(section);
-
 });
 
 const header = document.querySelector("header");
 
 window.addEventListener("scroll",()=>{
-
     if(window.scrollY>50){
-
         header.classList.add("scrolled");
-
     }else{
-
         header.classList.remove("scrolled");
-
     }
-
 });
 
 const menuToggle = document.getElementById("menuToggle");
-const nav = document.querySelector("mainNav");
+const mobileMenu = document.getElementById("mainNav");
+const overlay = document.getElementById("menuOverlay");
 
-menuToggle.addEventListener("click", () => {
-    nav.classList.toggle("active");
-});
+function toggleMenu(){
+    mobileMenu.classList.toggle("active");
+    overlay.classList.toggle("active");
+    menuToggle.textContent =
+        mobileMenu.classList.contains("active")
+        ? "✕"
+        : "☰";
+}
 
-document.querySelectorAll("#mainNav a").forEach(link => {
+menuToggle.addEventListener("click", toggleMenu);
+overlay.addEventListener("click", toggleMenu);
 
-    link.addEventListener("click", () => {
-        nav.classList.remove("active");
+document.querySelectorAll(".mobile-menu a").forEach(link=>{
+    link.addEventListener("click",()=>{
+        mobileMenu.classList.remove("active");
+        overlay.classList.remove("active");
+        menuToggle.textContent="☰";
     });
-
 });
